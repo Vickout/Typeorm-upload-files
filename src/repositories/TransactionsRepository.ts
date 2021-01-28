d39f313bf57c60328bb18abbf810354037fb71cf
Victor Lourenço <vlourenco.m@gmail.com>
2021-01-28 13:33:36 -0300

    Coded transaction repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,35 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const reducer = (accumulator: number, currentValue: number): number =>
+      accumulator + currentValue;
+
+    const incomeTransactions = transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        return transaction.value;
+      }
+      return 0;
+    });
+
+    const outcomeTransactions = transactions.map(transaction => {
+      if (transaction.type === 'outcome') {
+        return transaction.value;
+      }
+      return 0;
+    });
+
+    const income = incomeTransactions.reduce(reducer, 0);
+    const outcome = outcomeTransactions.reduce(reducer, 0);
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 }
 
