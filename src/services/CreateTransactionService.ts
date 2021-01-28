b64cea1ae06e951b1ddb2cd360411d0908b228e9
Victor Lourenço <vlourenco.m@gmail.com>
2021-01-27 23:44:39 -0300

    Create category and transaction services

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category_id: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category_id,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError('Transaction type invalid!');
+    }
+
+    // const { total } = transactionsRepository.getBalance();
+
+    // if (type === 'outcome' && total < value) {
+    //   throw new AppError('You do not have funds enough!');
+    // }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
